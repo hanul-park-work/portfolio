9540a82c991213e9c9861d5090c82d495ec8cb7e
haneul bak <parkh820@newschool.edu>
2021-08-20 14:50:15 +0900

    yodongkun

--- scripts.js
+++ scripts.js
@@ -12,3 +12,35 @@ for (i = 0; i < coll.length; i++) {
     }
   });
 }
+
+var inc = 0.01;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  frameRate(60);
+  pixelDensity(1);
+}
+
+function draw() {
+  clear();
+
+  const animation = map(millis(), 0, 1000, 0, 2 * PI);
+  const motion = sin(animation) * 0.5;
+
+  loadPixels();
+  var xoff = animation;
+  for (var x = 0; x < width; x++) {
+    var yoff = motion;
+    for (var y = 0; y < height; y++) {
+      var index = (x + y * width) * 4;
+      var r = noise(xoff + mouseX, yoff + mouseY) * 255;
+      pixels[index + 0] = r;
+      pixels[index + 1] = r;
+      pixels[index + 2] = r;
+      pixels[index + 3] = 255;
+      yoff += inc;
+    }
+    xoff += inc;
+  }
+  updatePixels();
+}
